bc14d5ec7387a7b1c69e6e46673b435fce569d18
Michael Beers <michael@michaelbeers.nl>
2026-08-07 15:03:52 +0200

    Cop classes from shadcn to checkbox component

--- registry/ui/checkbox.blade.php
+++ registry/ui/checkbox.blade.php
@@ -41,11 +41,11 @@
         :aria-checked="indeterminate ? 'mixed' : checked.toString()"
         @click="indeterminate ? ((indeterminate = false), (checked = true)) : (checked = ! checked)"
         @if ($disabled) disabled @endif
-        {{ $attributes->twMerge('peer border-input dark:bg-input/30 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-primary data-[state=checked]:border-primary data-[state=indeterminate]:bg-primary data-[state=indeterminate]:text-primary-foreground data-[state=indeterminate]:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-4 shrink-0 rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 flex items-center justify-center') }}
+        {{ $attributes->twMerge('peer size-4 shrink-0 rounded-[4px] border border-input shadow-xs transition-shadow outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 data-[state=checked]:border-primary data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground data-[state=indeterminate]:border-primary data-[state=indeterminate]:bg-primary data-[state=indeterminate]:text-primary-foreground dark:bg-input/30 dark:aria-invalid:ring-destructive/40 dark:data-[state=checked]:bg-primary dark:data-[state=indeterminate]:bg-primary') }}
     >
         <span
             data-slot="checkbox-indicator"
-            class="flex items-center justify-center text-current transition-none"
+            class="grid place-content-center text-current transition-none"
             x-show="checked || indeterminate"
             x-cloak
         >
