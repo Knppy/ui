2b636d8a05fe49630c5ec1d18fcba9e2f9c1cd7f
Michael Beers <michael@michaelbeers.nl>
2026-08-21 19:08:48 +0200

    Fix styles

--- registry/ui/carousel-next.blade.php
+++ registry/ui/carousel-next.blade.php
@@ -9,9 +9,11 @@
     :size="$size"
     x-bind:disabled="! canScrollNext"
     @click="scrollNext()"
-    x-bind:class="orientation === 'horizontal'
-        ? 'absolute top-1/2 -right-12 -translate-y-1/2'
-        : 'absolute -bottom-12 left-1/2 -translate-x-1/2 rotate-90'"
+    x-bind:class="
+        orientation === 'horizontal'
+            ? 'absolute top-1/2 -right-12 -translate-y-1/2'
+            : 'absolute -bottom-12 left-1/2 -translate-x-1/2 rotate-90'
+    "
     {{ $attributes->twMerge('size-8 rounded-full') }}
 >
     <x-lucide-arrow-right />
--- registry/ui/carousel-previous.blade.php
+++ registry/ui/carousel-previous.blade.php
@@ -9,9 +9,11 @@
     :size="$size"
     x-bind:disabled="! canScrollPrev"
     @click="scrollPrev()"
-    x-bind:class="orientation === 'horizontal'
-        ? 'absolute top-1/2 -left-12 -translate-y-1/2'
-        : 'absolute -top-12 left-1/2 -translate-x-1/2 rotate-90'"
+    x-bind:class="
+        orientation === 'horizontal'
+            ? 'absolute top-1/2 -left-12 -translate-y-1/2'
+            : 'absolute -top-12 left-1/2 -translate-x-1/2 rotate-90'
+    "
     {{ $attributes->twMerge('size-8 rounded-full') }}
 >
     <x-lucide-arrow-left />
