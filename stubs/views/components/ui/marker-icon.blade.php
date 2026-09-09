82061f228c62e305cd615655116ecb4cdc2535a7
Michael Beers <michael@michaelbeers.nl>
2026-09-09 11:40:42 +0200

    Add marker component

---
+++ stubs/views/components/ui/marker-icon.blade.php
@@ -0,0 +1,5 @@
+<span
+    data-slot="marker-icon"
+    aria-hidden="true"
+    {{ $attributes->twMerge(['class' => "size-4 shrink-0 [&_svg:not([class*='size-'])]:size-4"]) }}
+>{{ $slot }}</span>
---
+++ stubs/views/components/ui/marker.blade.php
@@ -0,0 +1,25 @@
+@props([
+    'is' => 'div',
+    'variant' => 'default',
+])
+
+@php
+    use Knppy\Ui\Facades\Ui;
+
+    $variants = [
+        'default' => '',
+        'separator' => 'before:mr-1 before:h-px before:min-w-0 before:flex-1 before:bg-border after:ml-1 after:h-px after:min-w-0 after:flex-1 after:bg-border',
+        'border' => 'border-b border-border pb-2',
+    ];
+    $classes = Ui::classes()
+        ->add("group/marker relative flex min-h-4 w-full items-center gap-2 text-left text-sm text-muted-foreground [&_svg:not([class*='size-'])]:size-4 [a]:underline [a]:underline-offset-3 [a]:hover:text-foreground")
+        ->add($variants[$variant] ?? $variants['default'])
+        ->add($attributes['class'] ?? '');
+@endphp
+
+<{{ $is }}
+    data-slot="marker"
+    data-variant="{{ $variant }}"
+    {{ $attributes->twMerge(['class' => $classes]) }}
+    >{{ $slot }}</{{ $is }}
+>
